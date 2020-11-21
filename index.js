4124381af89021b494a9f661f0c718402d3d97da
subho1999 <ghosh.subho.1999@gmail.com>
2020-11-21 11:08:26 +0530

    Add Decryption in local server

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ let keyArray = new Array();
 let namesArray = new Array();
 let encKeyArray = new Array();
 let decKeyArray = new Array();
+let decNamesArray = new Array();
+let origFile;
 
 const uploadFile = (file) => {
 	return new Promise((resolve, reject) => {
@@ -68,7 +70,7 @@ function encryptFile(filePath, index) {
 	namesArray.push(encFilePath);
 }
 
-function decryptFile(encFilePath, index) {
+function decryptFile(encFilePath, decFilePath, index) {
 	const readIV = fs.createReadStream(encFilePath, { end: 15 });
 	let initVector;
 	readIV.on("data", (chunk) => {
@@ -80,10 +82,12 @@ function decryptFile(encFilePath, index) {
 		const key = decKeyArray[index];
 		const decipher = crypto.createDecipheriv("aes256", key, initVector);
 
-		fs.unlinkSync(encFilePath.replace(".enc", ""));
+		// fs.unlinkSync(encFilePath.replace(".enc", ""));
 
-		const writeStream = fs.createWriteStream(encFilePath.replace(".enc", ""));
+		// const writeStream = fs.createWriteStream(encFilePath.replace(".enc", ""));
+		const writeStream = fs.createWriteStream(decFilePath);
 		readStream.pipe(decipher).pipe(unzip).pipe(writeStream);
+		decNamesArray.push(decFilePath);
 	});
 }
 
@@ -93,6 +97,7 @@ app.post("/upload", async (req, res) => {
 			res.status(400);
 			res.send("File not uploaded");
 		} else {
+			origFile = req.files.upload.name;
 			uploadFile(req.files.upload)
 				.then(() => {
 					splitFile
@@ -111,7 +116,8 @@ app.post("/upload", async (req, res) => {
 							keyArray.forEach((keyObject, index) => {
 								let encData = rsa_key.encryptPrivate(Buffer.from(keyObject.key), "base64");
 								encKeyArray.push(encData);
-							});+
+							});
+
 							// console.log(keyArray);
 							// console.log(encKeyArray);
 							// encKeyArray.forEach((rkey, index) => {
@@ -126,6 +132,7 @@ app.post("/upload", async (req, res) => {
 							// });
 
 							res.status(200);
+
 							res.send("Uploaded Files Sucessfully");
 						})
 						.catch((err) => {
@@ -145,6 +152,25 @@ app.post("/upload", async (req, res) => {
 	} catch (err) {
 		res.status(500).send(err);
 	}
+});
+
+app.get("/download", (req, res) => {
+	const publicKey = fs.readFileSync("publicKey.txt");
+	const dec_rsa_key = new NodeRSA();
+	dec_rsa_key.importKey(publicKey, "pkcs1-public-pem");
+	for (let i = 0; i < encKeyArray.length; i++) {
+		let decData = dec_rsa_key.decryptPublic(encKeyArray[i]);
+		decKeyArray.push(decData);
+		let decryptedFileName = namesArray[i].split("/").pop().replace(".enc", "");
+		let decryptedFilePath = path.join(__dirname, "downloads", decryptedFileName);
+
+		decryptFile(namesArray[i], decryptedFilePath, i);
+	}
+	let decryptedOutputFile = path.join(__dirname, "downloads", origFile);
+	setTimeout(() => {
+		splitFile.mergeFiles(decNamesArray, decryptedOutputFile);
+	}, 2000);
+	// console.log(decNamesArray);
 });
 
 const PORT = process.env.PORT || 5000;
